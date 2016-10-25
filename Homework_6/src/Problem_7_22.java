c66468f4951eb763eca36ddfde6e836e3e611880
John Paul Woods <woodsjp@student.swosu.edu>
2016-10-25 17:33:26 -0500

    added homework 6 problem 7.22

---
+++ Homework_6/src/Problem_7_22.java
@@ -0,0 +1,26 @@
+
+/*
+ * (Game: Eight Queens) The classic Eight Queens puzzle is 
+ * to place eight queens on a chess board such that no two 
+ * queens can attack each other (i.e., no two queens are on 
+ * the same row, same column, or same diagonal). There are 
+ * many possible solutions. Write a program that displays 
+ * one such solution. A sample output is shown below: 
+ * 
+ * |Q| | | | | | | | 
+ * | | | | |Q| | | | 
+ * | | | | | | | |Q| 
+ * | | | | | |Q| | | 
+ * | | |Q| | | | | | 
+ * | | | | | | |Q| | 
+ * | |Q| | | | | | | 
+ * | | | |Q| | | | |
+ */
+
+public class Problem_7_22 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
